5942715682008040cb7e964c0da7949ef1b9c156
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-25 05:31:09 +0900

    Initial Commit

---
+++ Swift5 ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5 ChatApp
+//
+//  Created by 平林宏淳 on 2020/05/25.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
