a53cac3cdd1dba5501979b4e9a029c955d88d1e2
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-29 04:37:03 +0900

    キーボードの設定(表示、隠す) TableViewController内のメッセージ設定を行いました。

--- Swift5 ChatApp/Controller/ChatViewController.swift
+++ Swift5 ChatApp/Controller/ChatViewController.swift
@@ -8,23 +8,112 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
-
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextFild: UITextField!
+    
+    //スクリーンのサイズ
+    let screenSize = UIScreen.main.bounds.size
+    var chatArray = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        messageTextFild.delegate = self
+        
+        //メッセージのセルを長さに合わせて可変にする
+        tableView.rowHeight = UITableView.automaticDimension
+        //初期の高さを決める
+        tableView.estimatedRowHeight = 85
+        
+        //キーボードの設定(出す)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillShow(_ :)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //キーボードの設定(隠す)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillHide(_ :)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        //Firebaseからfetchしてくる(取得)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func keyboardWillShow(_ notification:NSNotification){
+        
+        let keyboardHeight = ((notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as Any) as AnyObject).cgRectValue.height
+        
+        messageTextFild.frame.origin.y = screenSize.height - keyboardHeight - messageTextFild.frame.height
+        
+        
     }
-    */
-
+    
+    @objc func keyboardWillHide(_ notification:NSNotification){
+        
+        messageTextFild.frame.origin.y = screenSize.height - messageTextFild.frame.height
+        
+        guard let rect = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else{return}
+        
+        UIView.animate(withDuration: duration){
+            
+            let transform = CGAffineTransform(translationX: 0, y: 0)
+            self.view.transform = transform
+        }
+        
+    }
+    
+    //キーボード以外をタップで閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        messageTextFild.resignFirstResponder()
+    }
+    //キーボードをエンターで閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //メッセージの数
+        return chatArray.count
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.messageLabel.text = chatArray[indexPath.row].message
+        cell.userNameLabel.text = chatArray[indexPath.row].sender
+        cell.iconImageView.image = UIImage(named: "dogAvatarImage")
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ Swift5 ChatApp/Model/message.swift
@@ -0,0 +1,17 @@
+//
+//  message.swift
+//  Swift5 ChatApp
+//
+//  Created by 平林宏淳 on 2020/05/29.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import Foundation
+
+class Message{
+    
+    var sender:String = ""
+    var message:String = ""
+
+}
+
